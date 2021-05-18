fce8ec51e2e556b5f99928f2111df12ff87bbd48
Polynomeer <polynomeer@gmail.com>
2021-05-19 01:31:32 +0900

    build: Initialize gradle project (#2)
    Added dependencies for Spring Data JDBC, Spring Web, and MySQL.
    Added .gitignore for macOS, Eclipse, IntelliJ, and Gradle.

---
+++ backend/Airbnb/src/test/java/com/codesquad/team10/airbnb/AirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.team10.airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
