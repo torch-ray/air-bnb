7ecc04ce66109708e89b5ea71d97258bc123b80b
Polynomeer <polynomeer@gmail.com>
2021-05-19 02:29:59 +0900

    build: Add schema.sql to initialize DB schema (#2)
    Added schema.sql of which content is created by MySQL Workbench.

---
+++ backend/Airbnb/src/main/resources/schema.sql
@@ -0,0 +1,112 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
+SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
+        'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema airbnb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema airbnb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `airbnb` DEFAULT CHARACTER SET utf8;
+USE `airbnb`;
+
+-- -----------------------------------------------------
+-- Table `airbnb`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `airbnb`.`user`;
+
+CREATE TABLE IF NOT EXISTS `airbnb`.`user`
+(
+    `id`       VARCHAR(45) NOT NULL,
+    `password` VARCHAR(45) NULL,
+    `nickname` VARCHAR(45) NULL,
+    `email`    VARCHAR(45) NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `airbnb`.`room`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `airbnb`.`room`;
+
+CREATE TABLE IF NOT EXISTS `airbnb`.`room`
+(
+    `id`           INT         NOT NULL,
+    `location`     VARCHAR(45) NOT NULL,
+    `title`        VARCHAR(45) NOT NULL,
+    `x_pos`        VARCHAR(45) NOT NULL,
+    `y_pos`        VARCHAR(45) NOT NULL,
+    `ratings`      DOUBLE      NULL,
+    `reviews`      INT         NULL,
+    `charge`       INT         NOT NULL,
+    `cleaning_fee` INT         NULL,
+    `service_fee`  INT         NULL,
+    `tax_fee`      INT         NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `airbnb`.`wishlist`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `airbnb`.`wishlist`;
+
+CREATE TABLE IF NOT EXISTS `airbnb`.`wishlist`
+(
+    `user_id` VARCHAR(45) NOT NULL,
+    `room_id` INT         NOT NULL,
+    INDEX `fk_wishlist_user_idx` (`user_id` ASC) VISIBLE,
+    INDEX `fk_wishlist_room1_idx` (`room_id` ASC) VISIBLE,
+    CONSTRAINT `fk_wishlist_user`
+        FOREIGN KEY (`user_id`)
+            REFERENCES `airbnb`.`user` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `fk_wishlist_room1`
+        FOREIGN KEY (`room_id`)
+            REFERENCES `airbnb`.`room` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `airbnb`.`reserve`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `airbnb`.`reserve`;
+
+CREATE TABLE IF NOT EXISTS `airbnb`.`reserve`
+(
+    `user_id`   VARCHAR(45) NOT NULL,
+    `room_id`   INT         NOT NULL,
+    `check_in`  VARCHAR(45) NULL,
+    `check_out` VARCHAR(45) NULL,
+    `guests`    INT         NULL,
+    INDEX `fk_table1_user1_idx` (`user_id` ASC) VISIBLE,
+    INDEX `fk_table1_room1_idx` (`room_id` ASC) VISIBLE,
+    CONSTRAINT `fk_table1_user1`
+        FOREIGN KEY (`user_id`)
+            REFERENCES `airbnb`.`user` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `fk_table1_room1`
+        FOREIGN KEY (`room_id`)
+            REFERENCES `airbnb`.`room` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB;
+
+
+SET SQL_MODE = @OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
