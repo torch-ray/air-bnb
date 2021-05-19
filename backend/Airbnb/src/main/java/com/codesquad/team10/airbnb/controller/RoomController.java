327600d1be2e3062797bf1e94bcc83f1385db365
Polynomeer <polynomeer@gmail.com>
2021-05-19 23:26:32 +0900

    feat: Implement mock server for `GET /rooms` request (#6)
    Added RoomController::getSearchResult to process for `GET /rooms` request. It receives parameters and returns mock data for filtered search result using RoomDto and SearchResultDto.
    Added RoomDto and SearchResultDto to response for request `GET /rooms`.

--- backend/Airbnb/src/main/java/com/codesquad/team10/airbnb/controller/RoomController.java
+++ backend/Airbnb/src/main/java/com/codesquad/team10/airbnb/controller/RoomController.java
@@ -2,6 +2,8 @@ package com.codesquad.team10.airbnb.controller;
 
 import com.codesquad.team10.airbnb.dto.ChargeGroupDto;
 import com.codesquad.team10.airbnb.dto.ChargeRangeDto;
+import com.codesquad.team10.airbnb.dto.RoomDto;
+import com.codesquad.team10.airbnb.dto.SearchResultDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,4 +28,18 @@ public class RoomController {
         return new ChargeRangeDto(165556, chargeGroups);
     }
 
+    @GetMapping
+    public SearchResultDto getSearchResult(@RequestParam String location, @RequestParam("check-in") String checkIn, @RequestParam("check-out") String checkOut,
+                                           @RequestParam Integer min, @RequestParam Integer max, @RequestParam Integer guests) {
+        System.out.println(location + " " + checkIn + " " + checkOut + " " + min + " " + max + " " + guests);
+
+        List<RoomDto> rooms = new ArrayList<>();
+        rooms.add(new RoomDto("서울", "Spacious and Comfortable cozy house #4", "https://www.airbnb.com/rooms/29556662/photos/604588770?federated_search_id=6422bc81-ecb8-4381-a7b0-758e46144cbe&source_impression_id=p3_1621340285_vHikcFrealdXlffU&guests=1&adults=1",
+                "35.97664845766847", "126.99597295767953", "최대 인원 3명 ・ 원룸 ・ 침대 1개 ・ 욕실 1개・ 주방 ・ 무선 인터넷・ 에어컨 ・ 헤어드라이어", 82953, 149159, 4.5, 272));
+        rooms.add(new RoomDto("서울", "소소한 파티하기 좋은 숙소 / 홍대 / 연남동 / artist house3", "https://www.airbnb.com/rooms/29556662/photos/604588770?federated_search_id=6422bc81-ecb8-4381-a7b0-758e46144cbe&source_impression_id=p3_1621340285_vHikcFrealdXlffU&guests=1&adults=1",
+                "22.35364454353453", "85.68975653434279", "최대 인원 3명 ・ 원룸 ・ 침대 2개 ・ 욕실 1개・ 주방 ・ 무선 인터넷・ 에어컨 ・ 헤어드라이어", 72300, 132123, 3.5, 301));
+        rooms.add(new RoomDto("서울", "B) 3min from Itaewon Stn [ 2 BR ] Rooftop View", "https://www.airbnb.com/rooms/29556662/photos/604588770?federated_search_id=6422bc81-ecb8-4381-a7b0-758e46144cbe&source_impression_id=p3_1621340285_vHikcFrealdXlffU&guests=1&adults=1",
+                "127.23254443432437", "110.23232353534342", "최대 인원 2명 ・ 원룸 ・ 침대 1개 ・ 욕실 2개・ 주방 ・ 무선 인터넷・ 에어컨 ・ 헤어드라이어", 64302, 123030, 4.2, 500));
+        return new SearchResultDto(rooms.size(), rooms);
+    }
 }
