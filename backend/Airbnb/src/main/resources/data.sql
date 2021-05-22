bf9b1171ea705e1ae707faba3b4bc840e0684597
Polynomeer <polynomeer@gmail.com>
2021-05-22 22:29:11 +0900

    build: Add logging setting and data.sql (#8)
    Added logging setting to application.properties.
    Added data.sql and single insert query to it.

---
+++ backend/Airbnb/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `airbnb`.`room` (`id`, `location`, `title`, `x_pos`, `y_pos`, `ratings`, `reviews`, `charge`,
+                             `cleaning_fee`, `service_fee`, `tax_fee`)
+VALUES ('1', '서울', 'Spacious and Comfortable cozy house #4', '35.97664845766847', '126.99597295767953', 4.5, 272, 82953,
+        55948, 25996, 18247);
