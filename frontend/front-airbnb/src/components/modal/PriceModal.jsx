53e800b391f33ae779c4686d676dc3db2faba5f0
reminderclock <reminderclock@naver.com>
2021-05-31 21:43:41 +0900

    feat: rendering material ui range slider(#11)

--- frontend/front-airbnb/src/components/modal/PriceModal.jsx
+++ frontend/front-airbnb/src/components/modal/PriceModal.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import {Slider} from "@material-ui/core";
 
 const PriceModal = () => {
+    const gfg = [
+        {
+          value: 0,
+          label: "0",
+        },
+        {
+          value: 100,
+          label: "100",
+        },
+      ];
+      const [val, setVal] = useState([0, 100]);
+      const updateRange = (e, data) => {
+        setVal(data);
+      };
     return (
-        <PriceModalWrapper onClick={e => e.stopPropagation()}>
-
+        <PriceModalWrapper className="PriceModal" onClick={e => e.stopPropagation()}>
+            <PriceContainer>
+                <span> min/max : </span>
+                {val.map(e => <div>{e}</div>)}
+                <Slider value={val} onChange={updateRange} marks={gfg} />
+            </PriceContainer>{" "}
         </PriceModalWrapper>
     );
 }
 
 const PriceModalWrapper = styled.div`
     position: absolute;
-    width: 50%;
-    height: 500px;
+    width: 70%;
+    height: 300px;
     border-radius: 50px;
     background-color: #cfcfac;
     top:120%;
     left:20%;
+    color: green;
+  font-size: 16px;
+`;
+
+const PriceContainer = styled.div`
+/* text-align: center; */
+padding: 30px;
+width: 70%;
 `;
 export default PriceModal;
